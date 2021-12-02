c0a05838ec05fc33b65e5b77f6e626b0332e5295
Igor Jose <goncalvesigor67@gmail.com>
2021-12-02 19:57:52 -0300

    Crud Faculdade

--- src/main/java/br/com/igorjose/faculdade/controller/FaculdadeController.java
+++ src/main/java/br/com/igorjose/faculdade/controller/FaculdadeController.java
@@ -1,4 +1,88 @@
 package br.com.igorjose.faculdade.controller;
 
+import br.com.igorjose.faculdade.dto.MessageDTO;
+import br.com.igorjose.faculdade.models.Faculdade;
+import br.com.igorjose.faculdade.repository.FaculdadeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/faculdade")
 public class FaculdadeController {
+
+    private FaculdadeRepository faculdadeRepository;
+
+    @Autowired
+    public FaculdadeController(FaculdadeRepository faculdadeRepository) {
+        this.faculdadeRepository = faculdadeRepository;
+    }
+
+    @PostMapping
+    public MessageDTO cadastrarFaculdade(@RequestBody Faculdade faculdade) {
+
+        this.faculdadeRepository.save(faculdade);
+        return MessageDTO
+                .builder()
+                .message("Faculdade Cadastrada com sucesso!")
+                .build();
+    }
+
+    @GetMapping
+    public List<Faculdade> getFaculdades() {
+        List<Faculdade> faculdades = this.faculdadeRepository.findAll();
+        return faculdades;
+    }
+
+    @GetMapping("/{id}")
+    public Faculdade getFaculdadeById(@PathVariable Long id) {
+
+        Optional<Faculdade> faculdade = this.faculdadeRepository.findById(id);
+
+        if(faculdade.isPresent()) {
+            return faculdade.get();
+        }
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public MessageDTO updateFaculdade(@PathVariable Long id, @RequestBody Faculdade faculdade) {
+
+        Faculdade faculdadeToUpdate = this.faculdadeRepository.findById(id).get();
+
+        if(faculdadeToUpdate != null) {
+
+            faculdadeToUpdate.setCnpj(faculdade.getCnpj());
+            faculdadeToUpdate.setCursos(faculdade.getCursos());
+            faculdadeToUpdate.setNome(faculdade.getNome());
+            faculdadeToUpdate.setEndereco(faculdade.getEndereco());
+
+            this.faculdadeRepository.save(faculdadeToUpdate);
+            return MessageDTO.builder().message("Faculdade atualizada com sucesso").build();
+        }
+        return MessageDTO.builder().message("Id não encontrado").build();
+
+    }
+
+
+    @DeleteMapping("/{id}")
+    public MessageDTO deleteFaculdade(@PathVariable Long id) {
+
+        Faculdade faculdade = this.faculdadeRepository.findById(id).get();
+        if(faculdade != null) {
+
+            this.faculdadeRepository.delete(faculdade);
+            return MessageDTO
+                    .builder()
+                    .message("Faculdade deletada com sucesso!")
+                    .build();
+        }
+        this.faculdadeRepository.delete(faculdade);
+        return MessageDTO
+                .builder()
+                .message("Faculdade não encontrada!")
+                .build();
+    }
 }
--- src/main/java/br/com/igorjose/faculdade/repository/FaculdadeRepository.java
+++ src/main/java/br/com/igorjose/faculdade/repository/FaculdadeRepository.java
@@ -1,4 +1,8 @@
 package br.com.igorjose.faculdade.repository;
 
-public interface FaculdadeRepository {
+import br.com.igorjose.faculdade.models.Faculdade;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FaculdadeRepository extends JpaRepository<Faculdade, Long> {
+
 }
